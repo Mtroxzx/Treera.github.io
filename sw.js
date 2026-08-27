bbebffb0be1f44da4d09b5f50c801e44fb164fcc
Mtroxzx <hgdhfhgffff@gmail.com>
2026-08-27 14:52:49 +0700

    Refine mobile Measure interface

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'ra-ya-jing-v1';
+const CACHE_NAME = 'ra-ya-jing-v2';
 const APP_FILES = ['./', './index.html', './manifest.webmanifest', './icon.svg'];
 
 self.addEventListener('install', event => {
