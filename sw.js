31e817945237d1ecf4b6e523c8886facd852b9d8
Mtroxzx <hgdhfhgffff@gmail.com>
2026-08-27 14:56:19 +0700

    Rebuild app with iPhone Measure style

--- sw.js
+++ sw.js
@@ -1,16 +1,5 @@
-const CACHE_NAME = 'ra-ya-jing-v2';
-const APP_FILES = ['./', './index.html', './manifest.webmanifest', './icon.svg'];
-
-self.addEventListener('install', event => {
-  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_FILES)));
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', event => {
-  event.waitUntil(self.clients.claim());
-});
-
-self.addEventListener('fetch', event => {
-  if (event.request.method !== 'GET') return;
-  event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request)));
-});
+const CACHE='measure-iphone-style-v1';
+const FILES=['./','./index.html','./manifest.webmanifest','./icon.svg'];
+self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(FILES)));self.skipWaiting()});
+self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim()))});
+self.addEventListener('fetch',event=>{if(event.request.method==='GET')event.respondWith(caches.match(event.request).then(cached=>cached||fetch(event.request)))})
